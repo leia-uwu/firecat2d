9af27a7a38df97b24260cc82811b85997c2cbcf1
leia uwu <leia@tutamail.com>
2026-03-16 16:53:54 -0300

    chore: bump bundled SDL; remove pointless linking

--- cmake/findSDL.cmake
+++ cmake/findSDL.cmake
@@ -7,7 +7,7 @@ function(findSDL)
         FetchContent_Declare(
             SDL
             GIT_REPOSITORY https://github.com/libsdl-org/SDL
-            GIT_TAG        release-3.2.26
+            GIT_TAG        release-3.4.2
             GIT_SHALLOW ON
         )
         FetchContent_MakeAvailable(SDL)
